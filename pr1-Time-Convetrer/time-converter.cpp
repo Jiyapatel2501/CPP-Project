d98df8243ea318588c745165ec7a3cb2045d0cac
Jiyapatel2501 <pdrashti187@gmail.com>
2026-07-12 22:33:55 +0530

    Project 1 Time Converter System

--- pr1-Time-Convetrer/time-converter.cpp
+++ pr1-Time-Convetrer/time-converter.cpp
@@ -10,12 +10,14 @@ class time_convo{
 			cout<<"The hours of "<<sec<<" is :"<<h<<endl;
 			min=sec/60;
 			cout<<"The minutes of "<<sec<<" is :"<<min<<endl;
+			cout<<"----------------------------------"<<endl;
 		}
 		
 		void sec_count(int sec,int min,int h){
 			int ans;
 			ans=sec+(h*3600)+(min*60);
 			cout<<"The total second is :"<<ans<<endl;	
+			cout<<"----------------------------------"<<endl;
 		}	
 	
 };
@@ -24,28 +26,42 @@ int main(){
 	
 	time_convo t1;
 	int choice,sec,h,min;
-	cout<<"Press 1 to convert second into Minutes and hours"<<endl;
-	cout<<"Press 2 to Count total second"<<endl;
-	cout<<"Enter your choice :";
-	cin>>choice;
 	
-	switch(choice){
-		case 1:
-			cout<<"Enter seconds to convert :";
-			cin>>sec;
-			t1.sec_convo(sec);
-			break;
+	do{
+		cout<<"Press 1 to Convert second into Minutes and hours"<<endl;
+		cout<<"Press 2 to Count total second"<<endl;
+		cout<<"Press 3 to Exit"<<endl;
+		cout<<"Enter your choice :";
+		cin>>choice;
+		cout<<"----------------------------------"<<endl;
 		
-		case 2:
-			cout<<"Enter seconds to count :";
-			cin>>sec;
-			cout<<"Enter minutes to count :";
-			cin>>min;
-			cout<<"Enter hours to count :";
-			cin>>h;
-			t1.sec_count(sec,min,h);
-			break;
+		switch(choice){
+			case 1:
+				cout<<"Enter seconds to convert :";
+				cin>>sec;
+				t1.sec_convo(sec);
+				break;
 			
-	}	
+			case 2:
+				cout<<"Enter seconds to count :";
+				cin>>sec;
+				cout<<"Enter minutes to count :";
+				cin>>min;
+				cout<<"Enter hours to count :";
+				cin>>h;
+				t1.sec_count(sec,min,h);
+				break;
+				
+			case 3:
+				cout<<"Exiting From System..."<<endl;
+				break;
+				
+			default:
+				cout<<"Invalid Choice!"<<endl;
+				break;
+				
+		}	
+	}while(choice!=3);
+	
 	return 0;
 }
